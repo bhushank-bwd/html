105ce2e792b0534cce518ab0ec753021f80d2aa3
bhushanK <bhushank.bwd@gmail.com>
2023-02-21 20:49:54 +0530

    full calendar basic view created

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
-$(document).ready(function () {
-    
+$(document).ready(function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    headerToolbar: { center: "dayGridMonth,timeGridWeek,listWeek" },
+    views: {
+      dayGrid: {
+        // options apply to dayGridMonth, dayGridWeek, and dayGridDay views
+      },
+      timeGrid: {
+        // options apply to timeGridWeek and timeGridDay views
+      },
+      week: {
+        // options apply to dayGridWeek and timeGridWeek views
+      },
+      day: {
+        // options apply to dayGridDay and timeGridDay views
+      },
+      dayGridMonth: {
+        // name of view
+        titleFormat: { year: 'numeric', month: 'long' }  ,
+        // other view-specific options here
+      },
+    },
+  });
+  calendar.render();
 });
