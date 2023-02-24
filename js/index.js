0e2f7291edd7251970a487fa2aef2fb2453cb859
bhushanK <bhushank.bwd@gmail.com>
2023-02-24 21:40:47 +0530

    data-table with server side done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  $("#example").DataTable();
+  $("#example").DataTable({
+    serverSide: true,
+    ajax: {
+      url: "http://localhost:8080/ci3/get-all-users",
+      type: "GET",
+    },
+    order: [[1, "asc"]],
+    columnDefs: [{ orderable: false, targets: [0] }],
+    columns: [
+      { data: "srno" },
+      { data: "name" },
+      { data: "mob" },
+      { data: "email" },
+      { data: "address" },
+    ],
+  });
 });
