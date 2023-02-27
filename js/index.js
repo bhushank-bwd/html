672c2b65c4e9a55dac6821e62f4b126363bc6de1
bhushanK <bhushank.bwd@gmail.com>
2023-02-27 21:33:12 +0530

    form validate is not working

--- js/index.js
+++ js/index.js
@@ -69,6 +69,63 @@ $(document).ready(function () {
       }
     });
   });
+  $("#example").on("click", ".edit", function () {
+    let user_id = $(this).attr("data-id");
+    $.ajax({
+      url: "http://localhost:8080/ci3/get-user",
+      type: "POST",
+      data: { user_id },
+      dataType: "json",
+      success: function (response) {
+        if (response.status) {
+          console.log(response);
+          $("#name").val(response.user.name);
+          $("#address").val(response.user.address);
+          $("#email").val(response.user.email);
+          $("#mob").val(response.user.mob);
+          $("#user_id").val(user_id);
+          $("#modalTitleId").val("Update User");
+          $("#userModal").modal("show");
+        } else {
+          toastr.error(response.message);
+        }
+      },
+      error: function (jqXHR, exception) {
+        console.log(getError(jqXHR));
+      },
+    });
+  });
+  $("#user_action").validate({
+    errorElement: "span",
+    submitHandler: function (form) {
+      console.log("user_action");
+      var button_name = $("#submit_user")
+        .attr("disabled", false)
+        .html(btnLoading);
+      var formData = new FormData($("#user_action")[0]);
+      $.ajax({
+        url: base_url + "update-user",
+        type: "POST",
+        data: formData,
+        async: true,
+        dataType: "JSON",
+        success: function (response) {
+          console.log(response.status);
+          if (response.status) {
+            toastr.success(response.message);
+            // setTimeout(function () {
+            //   window.location.href = base_url + "admin-manage-cms/plan";
+            // }, 2000);
+          } else {
+            toastr.error(response.message);
+          }
+          $("#submit_user").attr("disabled", false).html("Submit");
+        },
+      });
+
+      return false;
+    },
+  });
 });
 function getError(jqXHR) {
   var msg = "";
