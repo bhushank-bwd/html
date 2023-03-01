0ec300b12c26d1a9a6ac1768f2a3e32067c0ebc1
bhushanK <bhushank.bwd@gmail.com>
2023-03-01 20:45:28 +0530

    crud done

--- js/index.js
+++ js/index.js
@@ -71,6 +71,7 @@ $(document).ready(function () {
   });
   $("#example").on("click", ".edit", function () {
     let user_id = $(this).attr("data-id");
+    resetForm();
     $.ajax({
       url: "http://localhost:8080/ci3/get-user",
       type: "POST",
@@ -84,7 +85,7 @@ $(document).ready(function () {
           $("#email").val(response.user.email);
           $("#mob").val(response.user.mob);
           $("#user_id").val(user_id);
-          $("#modalTitleId").val("Update User");
+          $("#modalTitleId").html("Update User");
           $("#userModal").modal("show");
         } else {
           toastr.error(response.message);
@@ -103,19 +104,31 @@ $(document).ready(function () {
         .attr("disabled", false)
         .html(btnLoading);
       var formData = new FormData($("#user_action")[0]);
+      let user_id = $("#user_id").val();
+      if (user_id == 0) {
+        route = "add-user";
+      } else {
+        route = "update-user";
+      }
+      console.log(form);
       $.ajax({
-        url: base_url + "update-user",
+        url: "http://localhost:8080/ci3/" + route,
         type: "POST",
         data: formData,
         async: true,
         dataType: "JSON",
+        cache: false,
+        contentType: false,
+        processData: false,
         success: function (response) {
           console.log(response.status);
+          resetForm();
+          $("#userModal").modal("hide");
           if (response.status) {
             toastr.success(response.message);
-            // setTimeout(function () {
-            //   window.location.href = base_url + "admin-manage-cms/plan";
-            // }, 2000);
+            setTimeout(function () {
+              location.reload();
+            }, 2000);
           } else {
             toastr.error(response.message);
           }
@@ -126,7 +139,16 @@ $(document).ready(function () {
       return false;
     },
   });
+  $("#add_user").click(function () {
+    resetForm();
+    $("#modalTitleId").html("Add User");
+    $("#userModal").modal("show");
+  });
 });
+function resetForm() {
+  $(".form-inputs").val("");
+  $("#user_id").val(0);
+}
 function getError(jqXHR) {
   var msg = "";
   if (jqXHR.status === 0) {
